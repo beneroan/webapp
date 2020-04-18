2f555d9277aa025ed017f400abc25526cc3e9ab0
Ben Taylor <bentaylor@bentaylor.me>
2020-04-18 14:42:27 -0500

    reminders

--- src/components/status.js
+++ src/components/status.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+const URL = 'http://localhost:5000'
+
 class Status extends Component {
    constructor (props) {
       super(props);
@@ -7,12 +9,13 @@ class Status extends Component {
       this.state = {
          loading: true,
          free: false,
-         lastFree: null
+         lastFree: null,
+         sentReminder: false
       }
    }
 
    componentDidMount () {
-      fetch('/status/machine?wing=' + this.props.wing + '&machine=' + this.props.machine)
+      fetch(URL + '/status/machine?wing=' + this.props.wing + '&machine=' + this.props.machine)
          .then(response => response.json())
          .then(data => {
             this.setState({
@@ -21,6 +24,15 @@ class Status extends Component {
                lastFree: data.lastFree ? new Date(data.lastFree) : new Date()
             })
          });
+   }
+
+   sendReminder () {
+      if (!this.state.sentReminder) {
+         this.setState({
+            sentReminder: true
+         })
+         fetch(URL + '/remind?wing=' + this.props.wing)
+      }
    }
 
    render = (props) => (
@@ -39,12 +51,35 @@ class Status extends Component {
             marginLeft: 10
          }}>{this.props.machine}</p>
          {
-            !this.state.free && !this.state.loading
-            ? <p style={{
-               marginTop: -6,
-               textAlign: 'right',
+            !this.state.free 
+            && !this.state.loading
+            && Math.floor(((new Date()) - this.state.lastFree) / 1000) != 0
+            ? 
+            <div style={{
                flexGrow: 1
-            }}>In use for {Math.floor(((new Date()) - this.state.lastFree) / 1000)} minutes</p>
+            }}>
+               <p style={{
+                  marginTop: -12,
+                  textAlign: 'right',
+                  width: '100%'
+               }}>
+                  In use for {Math.floor(((new Date()) - this.state.lastFree) / 1000)} minutes
+                  <span 
+                     style={{
+                        background: this.state.sentReminder ? '#999' : '#4285F4',
+                        borderRadius: 4,
+                        padding: 4,
+                        marginLeft: 8,
+                        cursor: 'pointer',
+                        color: '#fff',
+                        display: 'inline-block'
+                     }}
+                     onClick={this.sendReminder.bind(this)}
+                  >
+                     Send a Reminder
+                  </span>
+               </p>
+            </div>
             : null
          }
       </div>
